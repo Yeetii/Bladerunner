692c12bb492ef05086400127261ab64f753fbb11
Lukvargen <lukas.magnusson@hotmail.com>
2018-04-02 16:51:00 +0200

    changed player script from rigidbody to kinematicbody

--- Player.gd
+++ Player.gd
@@ -1,9 +1,9 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var speed = 10
+var speed = Vector2(0,-100)
 
 
 func _ready():
@@ -13,4 +13,4 @@ func _ready():
 	pass
 
 func _process(delta):
-	
+	move_and_collide(speed * delta);
