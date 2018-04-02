5db627502f06593df55155f7ca542286d2ac4882
Lukvargen <lukas.magnusson@hotmail.com>
2018-04-02 16:27:43 +0200

    First commit

---
+++ Player.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var speed = 10
+var gravity = 10
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	pass
+
+func _process(delta):
+	
