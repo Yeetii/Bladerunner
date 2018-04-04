698b9de0344a1ec89a4fcdc38ceb476c9b0d574b
Lukvargen <lukas.magnusson@hotmail.com>
2018-04-04 21:33:51 +0200

    added font

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -9,7 +9,5 @@ func _ready():
 
 func _on_Enemy_area_entered(area):
 	if area.get_parent().get_name() == "Sword":
-		print(area.get_parent().get_parent().can_attack)
-		if area.get_parent().get_parent().can_attack == false:
-			queue_free()
+		queue_free()
 
