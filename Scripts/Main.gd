16530ad329a8f68c5dfcf861cb664bac5f389cee
Yeetii <magnusserik@gmail.com>
2018-04-04 20:56:36 +0200

    Contin. Background

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,8 +1,27 @@
 extends Node
 
+var background = preload("res://Scenes/background.tscn")
+var old_b
+var new_b
+var last_pos = 0
+
 func _ready():
+	old_b = background.instance()
+	add_child(old_b)
+	
+	
+	new_b = background.instance()
+	add_child(new_b)
+	new_b.position.y = old_b.position.y - old_b.get_texture().get_size().y * old_b.get_transform().get_scale().y
 	pass
 
 func _process(delta):
-	if (int($Player.position.y) % 1000 <= 1):
-		print("passed")		 
+	#print(-int($Player.position.y) % 1000)
+	if ($Player.position.y < (last_pos - 854)):
+		last_pos = last_pos - 854
+
+		var temp = old_b
+		old_b = new_b
+		new_b = temp
+		new_b.position.y = old_b.position.y - old_b.get_texture().get_size().y * old_b.get_transform().get_scale().y
+		print(old_b.get_texture().get_size().y)
